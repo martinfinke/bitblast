2265dd79dbaf0de688e08863870f053072c4031d
Martin Finke <martin@basteln.local>
2015-05-20 17:23:40 +0200

    group a CNF/DNF by number of positive/negative literals

--- src/QuineMcCluskey.hs
+++ src/QuineMcCluskey.hs
@@ -1,7 +1,12 @@
 module QuineMcCluskey where
 
 import Formula
+import qualified Data.Map.Lazy as Map
+import Data.List(groupBy, sortBy)
+import Data.Ord(comparing)
+import Debug.Trace(traceShow)
 
+-- TODO: We should convert to the canonical form first!
 numRelevantLiterals :: Formula -> Int
 numRelevantLiterals formula
     | isMinterm formula = let (And literals) = formula in countPositive literals
@@ -9,3 +14,12 @@ numRelevantLiterals formula
     | otherwise = error $ "Not a min- or maxterm: " ++ show formula
     where countPositive = length . filter isPositiveLiteral
           countNegative = length . filter (not . isPositiveLiteral)
+
+groupByRelevantLiterals :: Formula -> Map.Map Int [Formula]
+groupByRelevantLiterals formula
+    | isCnf formula = let (And maxterms) = formula in toMap maxterms
+    | isDnf formula = let (Or minterms) = formula in toMap minterms
+    | otherwise = error $ "Not a CNF or DNF: " ++ show formula
+    where group terms = groupBy (\t1 t2 -> numRelevantLiterals t1 == numRelevantLiterals t2) terms :: [[Formula]]
+          withNumber = map (\terms@(term:_) -> (numRelevantLiterals term, terms))
+          toMap = Map.fromList . withNumber . group . sortBy (comparing numRelevantLiterals)
--- test/QuineMcCluskeySpec.hs
+++ test/QuineMcCluskeySpec.hs
@@ -5,6 +5,7 @@ import Control.Exception (evaluate)
 import QuineMcCluskey
 import TruthTable(var)
 import Formula
+import qualified Data.Map.Lazy as Map
 
 spec :: Spec
 spec = do
@@ -17,3 +18,26 @@ spec = do
 
         it "throws an error for a Formula that isn't a min/maxterm" $ do
             evaluate (numRelevantLiterals (Equiv [Atom (var 1)])) `shouldThrow` anyException
+
+    describe "groupByRelevantLiterals" $ do
+        it "groups a CNF by number of negative literals" $ do
+            let cnf = And [
+                    Or [Atom (var 0), Not $ Atom (var 1)],
+                    Or [Not $ Atom (var 0), Not $ Atom (var 1)],
+                    Or [Not $ Atom (var 0), Atom (var 1)]
+                    ]
+            groupByRelevantLiterals cnf `shouldBe` Map.fromList [
+                (1, [Or [Atom (var 0), Not $ Atom (var 1)], Or [Not $ Atom (var 0), Atom (var 1)]]),
+                (2, [Or [Not $ Atom (var 0), Not $ Atom (var 1)]])
+                ]
+
+        it "groups a DNF by number of negative literals" $ do
+            let dnf = Or [
+                    And [Atom (var 0), Not $ Atom (var 1)],
+                    And [Not $ Atom (var 0), Not $ Atom (var 1)],
+                    And [Not $ Atom (var 0), Atom (var 1)]
+                    ]
+            groupByRelevantLiterals dnf `shouldBe` Map.fromList [
+                (1, [And [Atom (var 0), Not $ Atom (var 1)], And [Not $ Atom (var 0), Atom (var 1)]]),
+                (0, [And [Not $ Atom (var 0), Not $ Atom (var 1)]])
+                ]
