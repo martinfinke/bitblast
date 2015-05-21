c9e0715821a474114e047531902d2df2878a23cc
Martin Finke <martin@basteln.local>
2015-05-21 07:43:15 +0200

    group terms into neighbouring pairs (based on number of pos/neg literals)

--- src/QuineMcCluskey.hs
+++ src/QuineMcCluskey.hs
@@ -1,5 +1,7 @@
 module QuineMcCluskey (numRelevantLiterals,
-                       groupByRelevantLiterals
+                       groupByRelevantLiterals,
+                       neighbourKeys,
+                       neighbourTerms
                        ) where
 
 import Formula (Formula(..))
@@ -27,3 +29,18 @@ groupTerms :: [Formula] -> Map.Map Int [Formula]
 groupTerms = Map.fromList . withNumber . group . sortBy (comparing numRelevantLiterals)
     where group terms = groupBy (\t1 t2 -> numRelevantLiterals t1 == numRelevantLiterals t2) terms :: [[Formula]]
           withNumber = map (\terms@(term:_) -> (numRelevantLiterals term, terms))
+
+neighbourKeys :: [Int] -> [(Int, Int)]
+neighbourKeys ints = [(i,i+1) | i <- ints, (i+1) `elem` ints]
+
+partitionTerms :: Map.Map Int [Formula] -> ([Formula], [Formula])
+partitionTerms termMap = (primeTerms, mergedTerms)
+    where primeTerms = undefined
+          mergedTerms = undefined
+          
+
+neighbourTerms :: Map.Map Int [Formula] -> [(Formula, Formula)]
+neighbourTerms termMap = concatMap allTermPairsForKeyPair neighbourKeyPairs
+    where neighbourKeyPairs = neighbourKeys $ Map.keys termMap :: [(Int, Int)]
+          allTermPairsForKeyPair (key1,key2) = [(term1,term2) | term1 <- lookupOrEmptyList key1, term2 <- lookupOrEmptyList key2]
+          lookupOrEmptyList key = maybe [] id (Map.lookup key termMap)
--- test/QuineMcCluskeySpec.hs
+++ test/QuineMcCluskeySpec.hs
@@ -4,7 +4,7 @@ import SpecHelper
 import Control.Exception (evaluate)
 import QuineMcCluskey
 import TruthTable(var)
-import Formula
+import Formula(Formula(..))
 import qualified Data.Map.Lazy as Map
 
 spec :: Spec
@@ -31,7 +31,7 @@ spec = do
                 (2, [Or [Not $ Atom (var 0), Not $ Atom (var 1)]])
                 ]
 
-        it "groups a DNF by number of negative literals" $ do
+        it "groups a DNF by number of positive literals" $ do
             let dnf = Or [
                     And [Atom (var 0), Not $ Atom (var 1)],
                     And [Not $ Atom (var 0), Not $ Atom (var 1)],
@@ -41,3 +41,45 @@ spec = do
                 (1, [And [Atom (var 0), Not $ Atom (var 1)], And [Not $ Atom (var 0), Atom (var 1)]]),
                 (0, [And [Not $ Atom (var 0), Not $ Atom (var 1)]])
                 ]
+
+    describe "neighbourKeys" $ do
+        it "is the empty list for an empty input list" $ do
+            neighbourKeys [] `shouldBe` []
+
+        it "is the empty list for an input list with just one element" $ do
+            neighbourKeys [5] `shouldBe` []
+
+        it "is one pair for a list with two neighbours" $ do
+            neighbourKeys [3,4] `shouldBe` [(3,4)]
+
+        it "is the empty list for an input list with two numbers that aren't neighbours" $ do
+            neighbourKeys [3,5] `shouldBe` []
+
+        it "is two pairs for a list with three adjacent numbers" $ do
+            neighbourKeys [6,7,8] `shouldBe` [(6,7), (7,8)]
+
+        it "is two pairs for a list with two neighbouring pairs which are apart from each other" $ do
+            neighbourKeys [3,4, 7,8] `shouldBe` [(3,4), (7,8)]
+
+    describe "neighbourTerms" $ do
+        let [x0,x1,x2] = map (Atom . var) [0,1,2]
+        let _0pos3neg = [Not x0, Not x1, Not x2]
+        let _1pos2neg = [x0, Not x1, Not x2]
+        let _2pos1neg_1diff = [x0, Not x1, x2]
+        let _2pos1neg_2diff = [Not x0, x1, x2]
+        let _3pos0neg = [x0, x1, x2]
+
+        it "is empty for an empty map" $ do
+            neighbourTerms Map.empty `shouldBe` []
+
+        it "is empty if there are no neighbour terms" $ do
+            let termMap = groupByRelevantLiterals $ And [Or _0pos3neg, Or _2pos1neg_1diff]
+            neighbourTerms termMap `shouldBe` []
+
+        it "is one pair if there are two neighbour terms" $ do
+            let termMap = groupByRelevantLiterals $ Or [And _1pos2neg, And _2pos1neg_1diff]
+            neighbourTerms termMap `shouldBe` [(And _1pos2neg, And _2pos1neg_1diff)]
+
+        it "doesn't care about the actual Hamming distance, only about the number of pos/neg literals" $ do
+            let termMap = groupByRelevantLiterals $ Or [And _1pos2neg, And _2pos1neg_2diff]
+            neighbourTerms termMap `shouldBe` [(And _1pos2neg, And _2pos1neg_2diff)]
