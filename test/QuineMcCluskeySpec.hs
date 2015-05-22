7b83adad2a767314fa60ae191b911729b51bd632
Martin Finke <martin@basteln.local>
2015-05-22 19:03:18 +0200

    verify that mergeTerms works as expected on examples

--- test/QuineMcCluskeySpec.hs
+++ test/QuineMcCluskeySpec.hs
@@ -6,6 +6,7 @@ import TruthTable(var)
 import Formula(Formula(..))
 import NormalForm(FormType(..))
 import qualified Data.Vector.Unboxed as V
+import Data.Maybe(isJust, catMaybes)
 
 
 instance Arbitrary QmcTerm where
@@ -150,7 +151,34 @@ spec = do
             mergeTerms (fromString "10") (fromString "11") `shouldBe` Just (fromString "1-")
             mergeTerms (fromString "1-0") (fromString "1-1") `shouldBe` Just (fromString "1--")
             mergeTerms (fromString "--10-110-1") (fromString "--10-100-1") `shouldBe` Just (fromString "--10-1-0-1")
-            
+
+    describe "Example Terms 1" $ do
+        -- Youtube: pQ3MfzqGlrc
+        let terms@[t2,t4,t5,t6,t7] = map fromString ["010", "100", "101", "110", "111"]
+        let afterFirstStep@[t2_6,t4_5,t4_6,t5_7,t6_7] = catMaybes $ map (uncurry mergeTerms) [(t2,t6), (t4,t5), (t4,t6), (t5,t7), (t6,t7)]
+        let afterSecondStep = catMaybes $ map (uncurry mergeTerms) [(t4_5,t6_7), (t4_6,t5_7)]
+
+        let expectedPrimes = map fromString ["-10", "1--", "1--"] -- not unique
+
+        it "merges the initial terms correctly" $ do
+            afterFirstStep `shouldBe` map fromString ["-10", "10-", "1-0", "1-1", "11-"]
+
+        it "merges correctly in the second step" $ do
+            afterSecondStep `shouldBe` map fromString ["1--", "1--"]
+
+    describe "Example Terms 2" $ do
+        -- Youtube: pQ3MfzqGlrc
+        let terms@[t0,t1,t2,t8,t3,t5,t10,t7,t14,t15] = map fromString ["0000", "0001", "0010", "1000", "0011", "0101", "1010", "0111", "1110", "1111"]
+        let afterFirstStep@[t0_1,t0_2,t0_8,t1_3,t1_5,t2_3,t2_10,t8_10,t3_7,t5_7,t10_14,t7_15,t14_15] = catMaybes $ map (uncurry mergeTerms) [(t0,t1), (t0,t2), (t0,t8), (t1,t3), (t1,t5), (t2,t3), (t2,t10), (t8,t10), (t3,t7), (t5,t7), (t10,t14), (t7,t15), (t14,t15)]
+        let afterSecondStep@[t0_1_2_3,t0_2_1_3,t0_2_8_10,t0_8_2_10,t1_3_5_7,t1_5_3_7] = catMaybes $ map (uncurry mergeTerms) [(t0_1,t2_3), (t0_2,t1_3), (t0_2,t8_10), (t0_8,t2_10), (t1_3,t5_7), (t1_5,t3_7)]
+
+        let expectedPrimes = [t10_14, t7_15, t14_15, t0_1_2_3, t0_2_1_3, t0_2_8_10, t0_8_2_10, t1_3_5_7, t1_5_3_7] -- not unique
+
+        it "merges the initial terms correctly" $ do
+            afterFirstStep `shouldBe` map fromString ["000-", "00-0", "-000", "00-1", "0-01", "001-", "-010", "10-0", "0-11", "01-1", "1-10", "-111", "111-"]
+
+        it "merges correctly in the second step" $ do
+            afterSecondStep `shouldBe` map fromString ["00--", "00--", "-0-0", "-0-0", "0--1", "0--1"]
 
     describe "neighbourKeys" $ do
         it "is the empty list for an empty input list" $ do
