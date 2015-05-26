e08aef5c43364d7549194bb11b7830530aa4f495
Martin Finke <martin@basteln.local>
2015-05-26 12:46:28 +0200

    added documentation for MinimizeFormula

--- src/MinimizeFormula.hs
+++ src/MinimizeFormula.hs
@@ -7,9 +7,11 @@ import qualified Data.Vector.Unboxed as U
 import QmTerm
 import Qm
 
-formulaToPrimesFormula :: Formula -> Formula
-formulaToPrimesFormula = minimizeCanonical . ensureCanonical
+-- | Minimizes an arbitrary 'Formula' to an equivalent CNF/DNF, which consists of the minimal cover of primes.
+minimizeFormula :: Formula -> Formula
+minimizeFormula = minimizeCanonical . ensureCanonical
 
+-- | Minimizes a 'Canonical' CNF or DNF using the Quine-McCluskey method.
 minimizeCanonical :: Canonical -> Formula
 minimizeCanonical canonical =
     let terms = canonicalToQmTerms canonical
@@ -36,6 +38,7 @@ termToQmTerm :: Int -- ^ The length the 'QmcTerm' should have
 termToQmTerm qmTermLength term = map QmTerm $ convertDashes (U.generate qmTermLength generator)
     where generator i = valueForVariable term (var i)
 
+-- | Replaces every dash ('Nothing') in the elements of a 'QmTerm' with two instances of the term: One with a 0, and one with a 1. This is done for all possible combinations, so if there are n dashes in a term, the output list has a length of 2^n.
 convertDashes :: U.Vector (Maybe Bool) -> [U.Vector (Maybe Bool)]
 convertDashes vec
     | U.null vec = [U.empty]
@@ -45,18 +48,7 @@ convertDashes vec
         where rest = convertDashes (U.tail vec)
               mapCons b = map (U.cons $ Just b)
 
--- | Converts a 'QmcTerm' back to a minterm (for DNFs) or maxterm (for CNFs). 
-qmTermToTerm :: Bool -- ^ 'True' for CNF, 'False' for DNF
-              -> QmTerm
-              -> Formula
-qmTermToTerm cnfMode (QmTerm vector) = op $ U.ifoldr translate [] vector
-    where op = if cnfMode then Or else And
-          translate i qmcTermElement rest = case qmcTermElement of
-                Just b -> invertIfCnf i b : rest
-                Nothing -> rest
-          invertIfCnf i b = if cnfMode == b then Not (Atom (var i)) else Atom (var i)
-
--- | Extracts the value of a 'Variable' in a term. Used to convert terms to 'QmcTerm's.
+-- | Extracts the value of a 'Variable' in a term. Used to convert terms to 'QmcTerm's. If the input 'Formula' is a CNF, the values are inverted (i.e. a positive literal results in a 0).
 valueForVariable :: Formula -> Variable -> QmTermEl
 valueForVariable term variable
     | Atom variable `elem` literals = Just $ if cnfMode then False else True
@@ -67,13 +59,31 @@ valueForVariable term variable
                 Or _ -> True
                 And _ -> False
 
-qmTermsToFormula :: Bool -> Int -> [QmTerm] -> Formula
+-- | Converts a list of 'QmTerm's back to a 'Formula'
+qmTermsToFormula :: Bool -- ^ Whether the 'Formula' was a CNF. If so, the terms will be inverted.
+                 -> Int -- ^ The original number of variables (i.e. the highest index + 1)
+                 -> [QmTerm]
+                 -> Formula
 qmTermsToFormula cnfMode originalTermLength qmTerms =
     let paddedQmTerms = map (padded originalTermLength) qmTerms
         terms = map (qmTermToTerm cnfMode) paddedQmTerms
         rootOp = if cnfMode then And else Or
     in  rootOp terms
 
-padded :: Int -> QmTerm -> QmTerm
+-- | Converts a 'QmcTerm' back to a minterm (for DNFs) or maxterm (for CNFs). 
+qmTermToTerm :: Bool -- ^ 'True' for CNF, 'False' for DNF. If true, the terms are inverted.
+              -> QmTerm
+              -> Formula
+qmTermToTerm cnfMode (QmTerm vector) = op $ U.ifoldr translate [] vector
+    where op = if cnfMode then Or else And
+          translate i qmcTermElement rest = case qmcTermElement of
+                Just b -> invertIfCnf i b : rest
+                Nothing -> rest
+          invertIfCnf i b = if cnfMode == b then Not (Atom (var i)) else Atom (var i)
+
+-- | Pads a 'QmTerm' with leading zeros ('False' values)
+padded :: Int -- ^ The length of the resulting QmTerm
+       -> QmTerm
+       -> QmTerm
 padded len (QmTerm vec) = QmTerm $ zeros U.++ vec
     where zeros = U.replicate (len - U.length vec) (Just False)
