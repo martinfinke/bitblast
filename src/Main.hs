34285605c26a27108fb2f4ca4bd904eff83ee14d
Martin Finke <martin@basteln.local>
2015-05-29 19:38:08 +0200

    main function calculates the minimized CNF for a range of bit widths

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,32 @@
 module Main where
 
-import TruthTable(var)
+import Arithmetics
 import Formula
-import NormalForm
 import MinimizeFormula
+import NormalForm
+import TruthTable
 
-[a2,a1,b2,b1,p8,p4,p2,p1] = map (Atom . var) [0..7]
+import Text.Printf(printf)
+import Control.Monad(forM_)
 
-twoBitMultDnf = Or [
-    And [Not a2, Not a1, Not b2, Not b1, Not p8, Not p4, Not p2, Not p1],
-    And [Not a2, Not a1, Not b2, b1, Not p8, Not p4, Not p2, Not p1],
-    And [Not a2, Not a1, b2, Not b1, Not p8, Not p4, Not p2, Not p1],
-    And [Not a2, Not a1, b2, b1, Not p8, Not p4, Not p2, Not p1],
-
-    And [Not a2, a1, Not b2, Not b1, Not p8, Not p4, Not p2, Not p1],
-    And [Not a2, a1, Not b2, b1, Not p8, Not p4, Not p2, p1],
-    And [Not a2, a1, b2, Not b1, Not p8, Not p4, p2, Not p1],
-    And [Not a2, a1, b2, b1, Not p8, Not p4, p2, p1],
-
-    And [a2, Not a1, Not b2, Not b1, Not p8, Not p4, Not p2, Not p1],
-    And [a2, Not a1, Not b2, b1, Not p8, Not p4, p2, Not p1],
-    And [a2, Not a1, b2, Not b1, Not p8, p4, Not p2, Not p1],
-    And [a2, Not a1, b2, b1, Not p8, p4, p2, Not p1],
-
-    And [a2, a1, Not b2, Not b1, Not p8, Not p4, Not p2, Not p1],
-    And [a2, a1, Not b2, b1, Not p8, Not p4, p2, Not p1],
-    And [a2, a1, b2, Not b1, Not p8, p4, p2, Not p1],
-    And [a2, a1, b2, b1, p8, Not p4, Not p2, p1]
-    ]
+nBitAddition :: Int -> Formula
+nBitAddition numBits =
+    let vars = map (Atom . var) [0..3*numBits + 1 - 1]
+        (xs,vars') = splitAt numBits vars
+        (ys,vars'') = splitAt numBits vars'
+        cOut = head vars''
+        sums = tail vars''
+        summerCircuit = summer xs ys cOut sums
+    in summerCircuit
 
 main :: IO ()
 main = do
-    let cnf = toCanonicalCnf twoBitMultDnf
-    let optimizedCnf = minimizeCanonical cnf
-    putStrLn $ show optimizedCnf
+    let bits = [1..5]
+    forM_ bits $ \bitWidth -> do
+        putStrLn $ printf "------------ %d-Bit Addition: --------------" bitWidth
+        putStrLn          "--------------------------------------------"
+        let circuit = nBitAddition bitWidth
+        let minimized = minimizeFormula circuit
+        putStrLn $ show minimized
+        putStrLn $ "Stats: " ++ show (getStats minimized)
+        putStrLn "\n\n\n"
