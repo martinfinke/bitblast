41d43ccf3de204a64dfd449f2ff9610084991e1d
Martin Finke <martin@basteln.local>
2015-05-29 09:56:16 +0200

    ArithmeticsSpec refactoring (removed duplication)

--- test/ArithmeticsSpec.hs
+++ test/ArithmeticsSpec.hs
@@ -8,24 +8,24 @@ import Control.Monad(forM_)
 
 spec :: Spec
 spec = do
+    let trueOnlyForAssignments assignments numVariables = setOutputs (zip assignments $ repeat (Just True)) (allFalseTable numVariables)
     describe "halfAdder" $ do
         it "has the correct truth table" $ do
             let [c,s,y,x] = map var [0..3]
             let ha = halfAdder (x,y) (s,c)
-            let shouldBeTrue = map assignmentFromString [
+            let trueAssignments = map assignmentFromString [
                     "0000",
                     "0110",
                     "1010",
                     "1101"
                     ]
-            let expectedTruthTable = setOutputs (zip shouldBeTrue $ repeat (Just True)) (allFalseTable 4)
-            toTruthTable ha `shouldBe` expectedTruthTable
+            toTruthTable ha `shouldBe` trueOnlyForAssignments trueAssignments 4
             
     describe "fullAdder" $ do
         it "has the correct truth table" $ do
             let [s,cOut,cIn,y,x] = map var [0..4]
             let fa = fullAdder (x,y) (cIn,cOut) s
-            let shouldBeTrue = map assignmentFromString [
+            let trueAssignments = map assignmentFromString [
                     "00000",
                     "00101",
                     "01001",
@@ -35,7 +35,6 @@ spec = do
                     "11010",
                     "11111"
                     ]
-            let expectedTruthTable = setOutputs (zip shouldBeTrue $ repeat (Just True)) (allFalseTable 5)
-            toTruthTable fa `shouldBe` expectedTruthTable
+            toTruthTable fa `shouldBe` trueOnlyForAssignments trueAssignments 5
 
             
