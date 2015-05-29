8f74c6ddda58e1c879dea2f219057034ebe74ae7
Martin Finke <martin@basteln.local>
2015-05-29 17:30:27 +0200

    separated between half/fullAdder (with connected outputs) and half/fullAdderSegment (without connected outputs)

--- src/Arithmetics.hs
+++ src/Arithmetics.hs
@@ -2,13 +2,42 @@ module Arithmetics where
 
 import Formula
 import TruthTable(Variable)
+import Data.List(zip4)
+import Debug.Trace(traceShow)
+
+halfAdderSegment :: (Formula,Formula) -> (Formula,Formula)
+halfAdderSegment (x,y) = (s,c)
+    where s = Xor [x,y]
+          c = And [x,y]
 
 halfAdder :: (Formula, Formula) -> (Formula, Formula) -> Formula
-halfAdder (x,y) (s,c) = And [assertC, assertS]
-    where assertC = Equiv [c, And [x,y]]
-          assertS = Equiv [s, Xor [x,y]]
+halfAdder (x,y) (s,c) = And [Equiv [s, s'], Equiv [c, c']]
+    where (s',c') = halfAdderSegment (x,y)
 
-fullAdder :: (Formula, Formula) -> (Formula, Formula) -> Formula -> Formula
-fullAdder (x,y) (cIn,cOut) s = And [assertCOut, assertS]
-    where assertCOut = Equiv [cOut, Or [And [y, cIn], And [x, cIn], And [x, y]]]
-          assertS = Equiv [s, Xor [x, y, cIn]]
+fullAdderSegment :: (Formula,Formula) -> Formula -> (Formula,Formula)
+fullAdderSegment (x,y) cIn = (s,cOut)
+    where s = Xor [x, y, cIn]
+          cOut = Or [And [y, cIn], And [x, cIn], And [x, y]]
+
+fullAdder :: (Formula,Formula) -> (Formula,Formula) -> Formula -> Formula
+fullAdder (x,y) (cIn,cOut) s = And [Equiv [s, s'], Equiv [cOut, cOut']]
+    where (s',cOut') = fullAdderSegment (x,y) cIn
+
+
+
+
+
+--makeSum :: (Formula,Formula,Formula) -> Formula
+--makeSum (x,y,c) = Xor [x, y, c]
+
+--calcCs :: (Formula, Formula) -> (Formula, [Formula]) -> (Formula, [Formula])
+--calcCs (x,y) (lastC, accum) =
+--    let current = Or [And [y, lastC], And [x, lastC], And [x, y]]
+--    in (current, current:accum)
+
+--summer :: [Formula] -> [Formula] -> (Formula, Formula) -> [Formula] -> Formula
+--summer xs ys (cIn,cOut) sums =
+--    let (cOut', cs) = foldr calcCs (cIn, cIn:[]) (zip xs ys)
+--        sums' = map makeSum (zip3 xs ys $ tail cs)
+--        sumsAreEqual = zipWith (\s1 s2 -> Equiv [s1, s2]) sums sums'
+--    in And $ Equiv [cOut, cOut'] : sumsAreEqual
--- test/ArithmeticsSpec.hs
+++ test/ArithmeticsSpec.hs
@@ -6,6 +6,7 @@ import Formula
 import TruthTable
 import Control.Monad(forM_)
 import MinimizeFormula
+import Debug.Trace(traceShow)
 
 spec :: Spec
 spec = do
@@ -21,11 +22,12 @@ spec = do
                     "1101"
                     ]
             toTruthTable ha `shouldBe` trueOnlyForAssignments trueAssignments 4
-            
-    describe "fullAdder" $ do
+
+    describe "fullAdderSegment" $ do
         let [s,cOut,cIn,y,x] = map (Atom . var) [0..4]
-        let fa = fullAdder (x,y) (cIn,cOut) s
-        it "has the correct truth table" $ do
+        let (s',cOut') = fullAdderSegment (x,y) cIn
+        let connectedOutputs = And [Equiv [s,s'], Equiv [cOut,cOut']]
+        it "has the correct truth table when the outputs are connected" $ do
             let trueAssignments = map assignmentFromString [
                     "00000",
                     "00101",
@@ -36,4 +38,63 @@ spec = do
                     "11010",
                     "11111"
                     ]
-            toTruthTable fa `shouldBe` trueOnlyForAssignments trueAssignments 5
+            toTruthTable connectedOutputs `shouldBe` trueOnlyForAssignments trueAssignments 5
+
+        it "is equivalent to a fullAdder when the outputs are connected" $ do
+            let fa = fullAdder (x,y) (cIn,cOut) s
+            toTruthTable connectedOutputs `shouldBe` toTruthTable fa
+
+    describe "summer" $ do
+        it "does" $ do
+            pending
+        --it "is equivalent to a fullAdder for 1 bit" $ do
+        --    let [s,cOut,cIn,y,x] = map (Atom . var) [0..4]
+        --    let fa = fullAdder (x,y) (cIn,cOut) s
+        --    let smer = summer [x] [y] (cIn,cOut) [s]
+        --    toTruthTable smer `shouldBe` toTruthTable fa
+
+        --it "has the correct truth table for 2 bits" $ do
+        --    let [x1,x0,y1,y0,cIn,cOut,s1,s0] = reverse $ map (Atom . var) [0..7]
+        --    let smer = summer [x1,x0] [y1,y0] (cIn,cOut) [s1,s0]
+        --    let trueAssignments = map assignmentFromString [
+        --            "00000000",
+        --            "00001001",
+        --            "00010001",
+        --            "00011010",
+        --            "00100010",
+        --            "11010100",
+        --            "11011101",
+        --            "11100101",
+        --            "11101110",
+        --            "11110110",
+        --            "11111111"
+        --            ]
+        --    let tt1 = toTruthTable smer 
+        --    let tt2 = trueOnlyForAssignments trueAssignments 8
+        --    let diff = truthTableDiff tt1 tt2
+        --    let expectedFormula = And [
+        --            Equiv [cOut, Or [And [y1, Or [And [y0, cIn], And [x0, cIn], And [x0, y0]]],
+        --                             And [x1, Or [And [y0, cIn], And [x0, cIn], And [x0, y0]]],
+        --                             And [x1, y1]]],
+        --            Equiv [s1, Xor [x1, y1, Or [And [y0, cIn], And [x0, cIn], And [x0, y0]]]],
+        --            Equiv [s0, Xor [x0, y0, cIn]]
+        --            ]
+        --    smer `shouldBe` expectedFormula
+        --    traceShow diff $ toTruthTable smer `shouldBe` trueOnlyForAssignments trueAssignments 8
+
+        --it "has the correct truth table for 4 bits" $ do
+            -- http://www.pradipyadav.com/2012/08/objective-to-design-and-implement-of-4.html
+            --let [a4,a3,a2,a1,b4,b3,b2,b1,c,s4,s3,s2,s1] = map (Atom . var) [0..12]
+            --let trueAssignments = map assignmentFromString [
+            --        "1000001001010",
+            --        "1000100010000",
+            --        "0010100001010",
+            --        "0001011101000",
+            --        "1010101110010",
+            --        "1110111111010",
+            --        "1010110110111"
+            --        ]
+
+            --let smer = summer [a4,a3,a2,a1] [b4,b3,b2,b1] (cIn,cOut) [s]
+            --toTruthTable smer `shouldBe` trueOnlyForAssignments trueAssignments 13
+            --pending
