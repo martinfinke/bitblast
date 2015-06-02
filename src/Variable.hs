73224785c75da0db5ed1c9107db503115b409fe9
Martin Finke <martin@basteln.local>
2015-06-02 05:38:06 +0200

    TruthTable type

--- src/Variable.hs
+++ src/Variable.hs
@@ -2,6 +2,7 @@ module Variable where
 
 import qualified Control.Monad.Trans.State.Lazy as State
 import qualified Data.IntMap as IntMap
+import qualified Data.Map as Map
 
 newtype VarMem = VarMem {currentVarIndex :: Int}
 type VarState = State.State VarMem
@@ -22,7 +23,7 @@ var varName = do
     return $ Variable varIndex
 
 newtype Assignment = Assignment (IntMap.IntMap Bool)
-    deriving(Eq, Show)
+    deriving(Eq, Show, Ord)
 
 emptyAssignment :: Assignment
 emptyAssignment = Assignment IntMap.empty
@@ -33,9 +34,17 @@ getVarValue (Variable i) (Assignment intMap) = IntMap.lookup i intMap
 setVarValue :: Variable -> Bool -> Assignment -> Assignment
 setVarValue (Variable i) b (Assignment intMap) = Assignment $ IntMap.insert i b intMap
 
+newtype TruthTable = TruthTable (Map.Map Assignment Bool)
+    deriving(Eq)
 
+emptyTable :: TruthTable
+emptyTable = TruthTable (Map.empty)
 
+getRow :: Assignment -> TruthTable -> Maybe Bool
+getRow assignment (TruthTable assignmentMap) = Map.lookup assignment assignmentMap
 
+setRow :: Assignment -> Bool -> TruthTable -> TruthTable
+setRow assignment b (TruthTable assignmentMap) = TruthTable $ Map.insert assignment b assignmentMap
 
 addVariable :: VarMem -> VarMem
 addVariable mem@(VarMem {currentVarIndex=idx}) = mem{currentVarIndex=succ idx}
--- test/VariableSpec.hs
+++ test/VariableSpec.hs
@@ -13,6 +13,7 @@ spec = do
     describe "var" $ do
         it "can create variables and keep them separate" $ do
             (test1 == test2) `shouldBe` False
+            (test1 == test3) `shouldBe` False
             (test2 == test3) `shouldBe` False
 
     describe "Assignment get/setVarValue" $ do
@@ -30,3 +31,16 @@ spec = do
             let a1 = setVarValue test1 True emptyAssignment
             let a2 = setVarValue test1 False emptyAssignment
             (a1 == a2) `shouldBe` False
+    describe "TruthTable get/setRow" $ do
+        let assignment1 = setVarValue test1 True emptyAssignment
+        let assignment2 = setVarValue test2 True emptyAssignment
+        let table1 = setRow assignment1 True emptyTable
+        let table2 = setRow assignment2 True emptyTable
+        it "is Nothing for an empty table" $ do
+            getRow emptyAssignment emptyTable `shouldBe` Nothing
+        it "is Just True for an assignment that has been set" $ do
+            getRow emptyAssignment (setRow emptyAssignment True emptyTable) `shouldBe` Just True
+        it "is Nothing if a different assignment has been set" $ do
+            getRow assignment1 (setRow assignment2 True emptyTable) `shouldBe` Nothing
+        it "creates two different tables if the rows being modified are different" $ do
+            (table1 == table2) `shouldBe` False
