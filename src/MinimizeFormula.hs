c4766a69a73690046e93f073b7d872e34a6c1f97
Martin Finke <martin@basteln.local>
2015-06-04 11:24:07 +0200

    MinimizeFormula doesn't use PositionMapping anymore

--- src/MinimizeFormula.hs
+++ src/MinimizeFormula.hs
@@ -10,24 +10,24 @@ import Qm
 type QmTermEl = Maybe Bool
 
 -- | Minimizes an arbitrary 'Formula' to an equivalent CNF/DNF, which consists of the minimal cover of primes.
-minimizeFormula :: PositionMapping -> Formula -> Formula
-minimizeFormula positionMapping formula =
+minimizeFormula :: Formula -> Formula
+minimizeFormula formula =
     let canonical = ensureCanonical formula
-    in minimizeCanonical positionMapping canonical
+    in minimizeCanonical canonical
 
 -- | Minimizes a 'Canonical' CNF or DNF using the Quine-McCluskey method.
-minimizeCanonical :: PositionMapping -> Canonical -> Formula
-minimizeCanonical positionMapping canonical =
-    let terms = canonicalToBitVectors varSet positionMapping canonical
+minimizeCanonical :: Canonical -> Formula
+minimizeCanonical canonical =
+    let terms = canonicalToBitVectors varSet canonical
         varSet = variableSet (getFormula canonical)
         cnfMode = (getType canonical == CNFType)
         minimumCover = qm terms [] []
     in case terms of
         [] -> if cnfMode then And [] else Or []
-        _ -> qmTermsToFormula varSet cnfMode positionMapping minimumCover
+        _ -> qmTermsToFormula varSet cnfMode minimumCover
 
-canonicalToBitVectors :: Set.Set Variable -> PositionMapping -> Canonical -> [BitVector]
-canonicalToBitVectors varSet positionMapping canonical = concatMap (convertDashes . packTerm varSet positionMapping) terms
+canonicalToBitVectors :: Set.Set Variable -> Canonical -> [BitVector]
+canonicalToBitVectors varSet canonical = concatMap (convertDashes . packTerm varSet) terms
     where terms = normalFormChildren formula
           formula = getFormula canonical
 
@@ -54,29 +54,28 @@ valueForVariable term variable
 -- | Converts a list of 'QmTerm's back to a 'Formula'
 qmTermsToFormula :: Set.Set Variable -- ^ The variable set of the formula
                  -> Bool -- ^ Whether the 'Formula' was a CNF. If so, the terms will be inverted.
-                 -> [Variable] -- ^ The Position Mapping
                  -> [QmTerm]
                  -> Formula
-qmTermsToFormula varSet cnfMode positionMapping qmTerms =
-    let terms = map (unpackTerm cnfMode varSet positionMapping) qmTerms
+qmTermsToFormula varSet cnfMode qmTerms =
+    let terms = map (unpackTerm cnfMode varSet) qmTerms
         rootOp = if cnfMode then And else Or
     in  rootOp terms
 
-packTerm :: Set.Set Variable -> PositionMapping -> Formula -> QmTerm
-packTerm variableSet posMapping term =
+packTerm :: Set.Set Variable -> Formula -> QmTerm
+packTerm varSet term =
     let emptyQmTerm = (QmTerm (0,0))
-        appearsInTerm = flip Set.member variableSet
-        selection = filter appearsInTerm (Set.toAscList variableSet)
+        appearsInTerm = flip Set.member varSet
+        selection = filter appearsInTerm (Set.toAscList varSet)
         variablesWithPositions = zip [0..] selection
         setBitForVariable (i,variable) (QmTerm (bv,mask)) = QmTerm $ case valueForVariable term variable of
             Nothing -> (bv, B.setBit mask i)
             Just bool -> (if bool then B.setBit bv i else B.clearBit bv i, mask)
     in foldr setBitForVariable emptyQmTerm variablesWithPositions
 
-unpackTerm :: Bool -> Set.Set Variable -> PositionMapping -> QmTerm -> Formula
-unpackTerm cnfMode variableSet posMapping (QmTerm (term,mask)) =
-    let appearsInTerm = flip Set.member variableSet
-        selection = filter appearsInTerm (Set.toAscList variableSet)
+unpackTerm :: Bool -> Set.Set Variable -> QmTerm -> Formula
+unpackTerm cnfMode varSet (QmTerm (term,mask)) =
+    let appearsInTerm = flip Set.member varSet
+        selection = filter appearsInTerm (Set.toAscList varSet)
         variablesWithPositions = zip [0..] selection
         op = if cnfMode then Or else And
         invertIfCnf v b = if cnfMode == b then Not (Atom v) else Atom v
