7ffa956f6db4c920feb38864dcc3329bf6b568e2
Martin Finke <martin@basteln.local>
2015-06-16 14:50:17 +0200

    added test case for two ones, two extra variables

--- src/TruthBased.hs
+++ src/TruthBased.hs
@@ -5,27 +5,38 @@ import Variable
 import Utils
 
 expand :: Int -> Set.Set Variable -> TruthTable -> [TruthTable]
-expand 0 _ table = [table]
-expand numExtraVars varSet table =
+expand numExtraVars varSet table
+    | numExtraVars < 0 = error $ "Number of extra variables can't be < 0: " ++ show numExtraVars
+    | numExtraVars == 0 = [table]
+    | otherwise = 
     let (branches, newVar) = expandOnOne table varSet
         newVarSet = Set.insert newVar varSet
     in concatMap (expand (numExtraVars-1) newVarSet) branches
 
 expandOnOne :: TruthTable -> Set.Set Variable -> ([TruthTable], Variable)
 expandOnOne table varSet =
-    let list = tableToList table
-        newVar = head $ newVariables varSet
-        trueAssignments = map fst . filter snd $ list
-        falseAssignments = map fst . filter (not . snd) $ list
-        branches = foldr (\trueAssignment bs -> concatMap (expandOne trueAssignment) bs) [[]] trueAssignments
-        expandOne trueAssignment branch =
-            let extraVarFalse = setVar newVar False trueAssignment
-                extraVarTrue = setVar newVar True trueAssignment
-            in [
-                (extraVarFalse, False) : (extraVarTrue, True) : branch,
-                (extraVarFalse, True) : (extraVarTrue, False) : branch,
-                (extraVarFalse, True) : (extraVarTrue, True) : branch
-                ]
-        falseRowsWithExtraVars = concat [[(setVar newVar False falseAssignment, False), (setVar newVar True falseAssignment, False)] | falseAssignment <- falseAssignments]
+    let newVar = head $ newVariables varSet
+        (trues, falses) = trueAndFalse table
+        branches = foldr (\trueAssignment bs -> concatMap (expandOne newVar trueAssignment) bs) [[]] trues
+        falseRowsWithExtraVars =
+            [(setVar newVar False false, False) | false <- falses] ++
+            [(setVar newVar True false, False) | false <- falses]
         withZeros branch = branch ++ falseRowsWithExtraVars
     in (map (tableFromList . withZeros) branches, newVar)
+
+trueAndFalse :: TruthTable -> ([Assignment], [Assignment])
+trueAndFalse table =
+    let list = tableToList table
+        trues = map fst . filter snd $ list
+        falses = map fst . filter (not . snd) $ list
+    in (trues, falses)
+
+expandOne :: Variable -> Assignment -> [(Assignment, Bool)] -> [[(Assignment, Bool)]]
+expandOne newVar trueAssignment branch =
+    let extraVarFalse = setVar newVar False trueAssignment
+        extraVarTrue = setVar newVar True trueAssignment
+    in [
+        (extraVarFalse, False) : (extraVarTrue, True) : branch,
+        (extraVarFalse, True) : (extraVarTrue, False) : branch,
+        (extraVarFalse, True) : (extraVarTrue, True) : branch
+        ]
--- test/TruthBasedSpec.hs
+++ test/TruthBasedSpec.hs
@@ -6,6 +6,7 @@ import Variable
 import Formula
 import qualified Data.Set as Set
 import Data.List(sort)
+import BranchTruthTables
 
 spec :: Spec
 spec = do
@@ -15,7 +16,7 @@ spec = do
     let varSet = Set.fromList vars
     let [x0,x1,x2,x3,x4,x5,x6,x7,x8,x9] = map Atom vars
     describe "expand" $ do
-        describe "when given a table with one true output" $ do
+        describe "when given a table with two variables and one true output" $ do
             let testVarSet = Set.fromList [v0,v1]
             let testTable = tableFromString testVarSet . unlines $ ["00 | 0", "01 | 0", "10 | 0", "11 | 1"]
             it "returns the input if no extra variables are allowed" $ do
@@ -23,40 +24,25 @@ spec = do
             it "returns 3 tables if 1 extra variable is allowed" $ do
                 let withExtraVar = Set.insert v2 testVarSet
                 let result = expand 1 testVarSet testTable
-                let segmentOne = ["000 | 0", "001 | 0", "010 | 0"]
-                let segmentTwo = ["100 | 0", "101 | 0", "110 | 0"]
-                let expected = map (tableFromString withExtraVar . unlines) [
-                        segmentOne ++ ["011 | 0"] ++ segmentTwo ++ ["111 | 1"],
-                        segmentOne ++ ["011 | 1"] ++ segmentTwo ++ ["111 | 0"],
-                        segmentOne ++ ["011 | 1"] ++ segmentTwo ++ ["111 | 1"]
-                        ]
+                let expected = map (tableFromString withExtraVar . unlines) twoVars_oneOne_oneExtra
                 result `shouldBe` expected
             it "doesn't matter if called twice with 1, or once with 2" $ do
                 let whenCalledTwice = concatMap (expand 1 (Set.insert v2 testVarSet)) $ expand 1 testVarSet testTable
                 let whenCalledOnce = expand 2 testVarSet testTable
                 whenCalledTwice `shouldBe` whenCalledOnce
-        describe "when given a table with just one variable" $ do
+        describe "when given a table with one variable and one true output" $ do
             it "returns 15 tables if 2 extra variables are allowed" $ do
                 let testVarSet = Set.singleton v0
                 let smallTable = tableFromString testVarSet . unlines $ ["0 | 1", "1 | 0"]
                 let withExtraVars = Set.insert v2 $ Set.insert v1 testVarSet
                 let result = sort $ expand 2 testVarSet smallTable
-                let common = ["011 | 0", "101 | 0", "111 | 0", "001 | 0"]
-                let expected = sort $ map (tableFromString withExtraVars . unlines) [
-                        ["000 | 0", "010 | 0", "100 | 0", "110 | 1"] ++ common,
-                        ["000 | 0", "010 | 0", "100 | 1", "110 | 0"] ++ common,
-                        ["000 | 0", "010 | 0", "100 | 1", "110 | 1"] ++ common,
-                        ["000 | 0", "010 | 1", "100 | 0", "110 | 0"] ++ common,
-                        ["000 | 0", "010 | 1", "100 | 0", "110 | 1"] ++ common,
-                        ["000 | 0", "010 | 1", "100 | 1", "110 | 0"] ++ common,
-                        ["000 | 0", "010 | 1", "100 | 1", "110 | 1"] ++ common,
-                        ["000 | 1", "010 | 0", "100 | 0", "110 | 0"] ++ common,
-                        ["000 | 1", "010 | 0", "100 | 0", "110 | 1"] ++ common,
-                        ["000 | 1", "010 | 0", "100 | 1", "110 | 0"] ++ common,
-                        ["000 | 1", "010 | 0", "100 | 1", "110 | 1"] ++ common,
-                        ["000 | 1", "010 | 1", "100 | 0", "110 | 0"] ++ common,
-                        ["000 | 1", "010 | 1", "100 | 0", "110 | 1"] ++ common,
-                        ["000 | 1", "010 | 1", "100 | 1", "110 | 0"] ++ common,
-                        ["000 | 1", "010 | 1", "100 | 1", "110 | 1"] ++ common
-                        ]
+                let expected = sort $ map (tableFromString withExtraVars . unlines) oneVar_oneOne_twoExtra
+                result `shouldBe` expected
+        describe "when given a table with two ones" $ do
+            it "returns x tables if 2 extra variables are allowed" $ do
+                let testVarSet = Set.singleton v0
+                let smallTable = tableFromString testVarSet . unlines $ ["0 | 1", "1 | 1"]
+                let withExtraVars = Set.insert v2 $ Set.insert v1 testVarSet
+                let result = sort $ expand 2 testVarSet smallTable
+                let expected  = sort $ map (tableFromString withExtraVars . unlines) oneVar_twoOnes_twoExtra
                 result `shouldBe` expected
